c97ff69abcf4d6c348f2827dff8744b6c986f6fa
Michael Graupner <michael.graupner@parisdescartes.fr>
2016-06-02 11:50:54 +0200

    introduced cases in irregularPairs_Ca-Model.py such that only one scenario is calculated per simulation

--- irregularPairs_Ca-Model.py
+++ irregularPairs_Ca-Model.py
@@ -63,178 +63,258 @@ else:
     print "No parameter set specified!"
     sys.exit(0)
 
+if len(sys.argv[2]) > 0:
+    case = sys.argv[2]
+else:
+    print 'No case specified!'
+    print 'Chose one : deltaTFreq, deltaTP, freqDeltaT'
+    sys.exit(0)
+    
 
+
+    
 ##########################################################
 # output directory
 outputDir = 'simResults/'
 
+# numerical integration step width
+deltaCa = 0.005 #0.01 #  0.0001 
+    
 ##################################################################################################
 # synaptic change vs Delta T for irregular Pairs
 ##################################################################################################
-# Parameter of the stimulation protocol
-frequencies   = array([1.,5.,10.,20.,30.,50.])   # frequency of spike-pair presentations in pairs/sec
-T_total     = 10.     # total time of stimulation in sec
-DeltaTstart = -0.1    # start time difference between pre- and post-spike, in sec
-DeltaTend   =  0.1    # end time difference between pre- and post-spike, in sec
-DeltaTsteps =  101.  # steps between start and end value
-ppp         = 1.
-rho0        = 0.5
-
-# nonlinearity factor
-nl = 1.
-
-# numerical integration step width
-deltaCa = 0.005 #0.01 #  0.0001 
-
-# parameter-set to use
-#params = 'solOld'
-
-nCases = len(frequencies)
-
-
-###########################################################
-# initiate synaptic change class and chose parameter set from file
-synChange = synapticChange(params,fromFile=True,nonlinear=nl)
-# initiate class to calculate fraction of time above threshold
-tat = timeAboveThreshold(synChange.tauCa, synChange.Cpre, synChange.Cpost, synChange.thetaD, synChange.thetaP, nonlinear=nl)
-
-
-###########################################################
-# initialize arrays 
-
-deltaT = linspace(DeltaTstart,DeltaTend,DeltaTsteps)
-
-pool = multiprocessing.Pool()
-
-resultsIrr = zeros(len(frequencies)*3+2)
-
-###########################################################
-# simulation loop over range of deltaT values
-for i in range(len(deltaT)):
-    #
-    print 'deltaT : ', deltaT[i]
+if case == 'deltaTFreq':
     
-    args = column_stack((ones(nCases)*deltaT[i],frequencies,frequencies,ones(nCases)*ppp))
+    # Parameter of the stimulation protocol
+    frequencies   = array([1.,5.,10.,20.,30.,50.])   # frequency of spike-pair presentations in pairs/sec
+    T_total     = 10.     # total time of stimulation in sec
+    DeltaTstart = -0.1    # start time difference between pre- and post-spike, in sec
+    DeltaTend   =  0.1    # end time difference between pre- and post-spike, in sec
+    DeltaTsteps =  101.  # steps between start and end value
+    ppp         = 1.
+    rho0        = 0.5
 
-    rrr = pool.map(runIrregularPairSimulations,args)
-    #for n in range(len(frequencies)):
-    #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
-    #pdb.set_trace()
-    res1 = hstack((deltaT[i],frequencies,frequencies,ppp,rrr))
-    resultsIrr = vstack((resultsIrr,res1))
+    # nonlinearity factor
+    nl = 1.
 
-resultsIrr = resultsIrr[1:]
+    nCases = len(frequencies)
 
-# the array has to be flipped if Cpre>Cpost 
-if synChange.Cpre>synChange.Cpost:
-    # invert time axis
-    resultsIrr[:,0] = resultsIrr[:,0][::-1] 
-    # reorder entire array to end up with increasing time
-    resultsIrr = resultsIrr[::-1]
+
+    ###########################################################
+    # initiate synaptic change class and chose parameter set from file
+    synChange = synapticChange(params,fromFile=True,nonlinear=nl)
+    # initiate class to calculate fraction of time above threshold
+    tat = timeAboveThreshold(synChange.tauCa, synChange.Cpre, synChange.Cpost, synChange.thetaD, synChange.thetaP, nonlinear=nl)
+
+
+    ###########################################################
+    # initialize arrays 
+
+    deltaT = linspace(DeltaTstart,DeltaTend,DeltaTsteps)
+
+    pool = multiprocessing.Pool()
+
+    resultsIrr = zeros(len(frequencies)*3+2)
+
+    ###########################################################
+    # simulation loop over range of deltaT values
+    for i in range(len(deltaT)):
+        #
+        print 'deltaT : ', deltaT[i]
+        
+        args = column_stack((ones(nCases)*deltaT[i],frequencies,frequencies,ones(nCases)*ppp))
+
+        rrr = pool.map(runIrregularPairSimulations,args)
+        #for n in range(len(frequencies)):
+        #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
+        #pdb.set_trace()
+        res1 = hstack((deltaT[i],frequencies,frequencies,ppp,rrr))
+        resultsIrr = vstack((resultsIrr,res1))
+
+    resultsIrr = resultsIrr[1:]
+
+    # the array has to be flipped if Cpre>Cpost 
+    if synChange.Cpre>synChange.Cpost:
+        # invert time axis
+        resultsIrr[:,0] = resultsIrr[:,0][::-1] 
+        # reorder entire array to end up with increasing time
+        resultsIrr = resultsIrr[::-1]
+        
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
+        
+    np.save(outputDir+'irregularSpikePairs_vs_deltaT_differentFreq_%s.npy' % params,resultsIrr)
+    np.savetxt(outputDir+'irregularSpikePairs_vs_deltaT_differentFreq_%s.dat' % params,resultsIrr)
+
+
+    ##########################################################
+    # synaptic change vs Delta T for regular Pairs
+    ##########################################################
+    # Parameter of the stimulation protocol
+    frequencies   = array([1.,5.,10.,20.,30.,50.])   # frequency of spike-pair presentations in pairs/sec
+    T_total     = 10.     # total time of stimulation in sec
+    DeltaTstart = -0.1    # start time difference between pre- and post-spike, in sec
+    DeltaTend   =  0.1    # end time difference between pre- and post-spike, in sec
+    DeltaTsteps =  101.  # steps between start and end value
+    ppp         = 1.
+    rho0        = 0.5
+
+    # nonlinearity factor
+    nl = 1.
+
+    nCases = len(frequencies)
+
+
+    ###########################################################
+    # initialize arrays 
+
+    deltaT = linspace(DeltaTstart,DeltaTend,DeltaTsteps)
+
+    resultsReg = zeros(len(frequencies)*3+2)
+
+    ###########################################################
+    # simulation loop over range of deltaT values
+    for i in range(len(deltaT)):
+        #
+        print 'deltaT : ', deltaT[i]
+        
+        args = column_stack((ones(nCases)*deltaT[i],frequencies,frequencies,ones(nCases)*ppp))
+
+        rrr = pool.map(runRegularPairSimulations,args)
+        #for n in range(len(frequencies)):
+        #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
+        #pdb.set_trace()
+        res1 = hstack((deltaT[i],frequencies,frequencies,ppp,rrr))
+        resultsReg = vstack((resultsReg,res1))
+
+    resultsReg = resultsReg[1:]
+
+    np.save(outputDir+'regularSpikePairs_vs_deltaT_%s.npy' % params,resultsReg)
+    np.savetxt(outputDir+'regularSpikePairs_vs_deltaT_%s.dat' % params,resultsReg)
+
+##################################################################################################
+# synaptic change vs Delta T for irregular Pairs
+##################################################################################################
+if case == 'deltaTP':
     
-if not os.path.exists(outputDir):
-    os.makedirs(outputDir)
-    
-np.save(outputDir+'irregularSpikePairs_vs_deltaT_%s.npy' % params,resultsIrr)
-np.savetxt(outputDir+'irregularSpikePairs_vs_deltaT_%s.dat' % params,resultsIrr)
+    # Parameter of the stimulation protocol
+    frequency   = 10.     # frequency of spike-pair presentations in pairs/sec
+    T_total     = 10.     # total time of stimulation in sec
+    DeltaTstart = -0.1    # start time difference between pre- and post-spike, in sec
+    DeltaTend   =  0.1    # end time difference between pre- and post-spike, in sec
+    DeltaTsteps =  101.  # steps between start and end value
+    ppp         = array([0.1,0.25,0.5,0.75,1.])
+    rho0        = 0.5
 
-##########################################################
-# synaptic change vs Delta T for regular Pairs
-##########################################################
-# Parameter of the stimulation protocol
-frequencies   = array([1.,5.,10.,20.,30.,50.])   # frequency of spike-pair presentations in pairs/sec
-T_total     = 10.     # total time of stimulation in sec
-DeltaTstart = -0.1    # start time difference between pre- and post-spike, in sec
-DeltaTend   =  0.1    # end time difference between pre- and post-spike, in sec
-DeltaTsteps =  101.  # steps between start and end value
-ppp         = 1.
-rho0        = 0.5
+    # nonlinearity factor
+    nl = 1.
 
-# nonlinearity factor
-nl = 1.
-
-nCases = len(frequencies)
+    nCases = len(ppp)
 
 
-###########################################################
-# initialize arrays 
+    ###########################################################
+    # initiate synaptic change class and chose parameter set from file
+    synChange = synapticChange(params,fromFile=True,nonlinear=nl)
+    # initiate class to calculate fraction of time above threshold
+    tat = timeAboveThreshold(synChange.tauCa, synChange.Cpre, synChange.Cpost, synChange.thetaD, synChange.thetaP, nonlinear=nl)
 
-deltaT = linspace(DeltaTstart,DeltaTend,DeltaTsteps)
 
-resultsReg = zeros(len(frequencies)*3+2)
+    ###########################################################
+    # initialize arrays 
 
-###########################################################
-# simulation loop over range of deltaT values
-for i in range(len(deltaT)):
-    #
-    print 'deltaT : ', deltaT[i]
-    
-    args = column_stack((ones(nCases)*deltaT[i],frequencies,frequencies,ones(nCases)*ppp))
+    deltaT = linspace(DeltaTstart,DeltaTend,DeltaTsteps)
 
-    rrr = pool.map(runRegularPairSimulations,args)
-    #for n in range(len(frequencies)):
-    #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
-    #pdb.set_trace()
-    res1 = hstack((deltaT[i],frequencies,frequencies,ppp,rrr))
-    resultsReg = vstack((resultsReg,res1))
+    pool = multiprocessing.Pool()
 
-resultsReg = resultsReg[1:]
+    resultsIrr = zeros(len(ppp)*2+3)
 
-np.save(outputDir+'regularSpikePairs_vs_deltaT_%s.npy' % params,resultsReg)
-np.savetxt(outputDir+'regularSpikePairs_vs_deltaT_%s.dat' % params,resultsReg)
+    ###########################################################
+    # simulation loop over range of deltaT values
+    for i in range(len(deltaT)):
+        #
+        print 'deltaT : ', deltaT[i]
+        
+        args = column_stack((ones(nCases)*deltaT[i],ones(nCases)*frequency,ones(nCases)*frequency,ppp))
 
+        rrr = pool.map(runIrregularPairSimulations,args)
+        #for n in range(len(frequencies)):
+        #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
+        #pdb.set_trace()
+        res1 = hstack((deltaT[i],frequency,frequency,ppp,rrr))
+        resultsIrr = vstack((resultsIrr,res1))
+
+    resultsIrr = resultsIrr[1:]
+
+    # the array has to be flipped if Cpre>Cpost 
+    if synChange.Cpre>synChange.Cpost:
+        # invert time axis
+        resultsIrr[:,0] = resultsIrr[:,0][::-1] 
+        # reorder entire array to end up with increasing time
+        resultsIrr = resultsIrr[::-1]
+        
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
+        
+    np.save(outputDir+'irregularSpikePairs_vs_deltaT_differentP_%s.npy' % params,resultsIrr)
+    np.savetxt(outputDir+'irregularSpikePairs_vs_deltaT_differenP_%s.dat' % params,resultsIrr)
 
 
 ##################################################################################################
-# synaptic change vs frequency
+# synaptic change vs rate
 ##################################################################################################
+elif case == 'freqDeltaT' :
+    deltaTs   = array([-0.01,-0.01,0.,0.01,0.01])   # frequency of spike-pair presentations in pairs/sec
+    T_total     = 10.     # total time of stimulation in sec
+    Freqstart = 1.    # start time difference between pre- and post-spike, in sec
+    FreqTend   =  60.    # end time difference between pre- and post-spike, in sec
+    FreqSteps =  60  # steps between start and end value
+    ppp         = array([1.,0.4,0.,0.4,1.])
+    rho0        = 0.5
 
-deltaTs   = array([-0.01,-0.01,0.,0.01,0.01])   # frequency of spike-pair presentations in pairs/sec
-T_total     = 10.     # total time of stimulation in sec
-Freqstart = 1.    # start time difference between pre- and post-spike, in sec
-FreqTend   =  60.    # end time difference between pre- and post-spike, in sec
-FreqSteps =  60  # steps between start and end value
-ppp         = array([1.,0.4,0.,0.4,1.])
-rho0        = 0.5
-
-# nonlinearity factor
-nl = 1.
-
-
-nCases = len(deltaTs)
+    # nonlinearity factor
+    nl = 1.
 
 
-###########################################################
-# initialize arrays 
+    nCases = len(deltaTs)
 
-frequencies = linspace(Freqstart,FreqTend,FreqSteps)
 
-results = zeros(len(deltaTs)*3+2)
+    ###########################################################
+    # initialize arrays 
 
-###########################################################
-# simulation loop over range of deltaT values
-for i in range(len(frequencies)):
-    #
-    print 'rate : ', frequencies[i]
-    
-    args = column_stack((deltaTs,ones(nCases)*frequencies[i],ones(nCases)*frequencies[i],ppp))
+    frequencies = linspace(Freqstart,FreqTend,FreqSteps)
 
-    rrr = pool.map(runIrregularPairSimulations,args)
-    #for n in range(len(frequencies)):
-    #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
-    #pdb.set_trace()
-    res1 = hstack((deltaTs,frequencies[i],frequencies[i],ppp,rrr))
-    results = vstack((results,res1))
+    results = zeros(len(deltaTs)*3+2)
 
-results = results[1:]
+    ###########################################################
+    # simulation loop over range of deltaT values
+    for i in range(len(frequencies)):
+        #
+        print 'rate : ', frequencies[i]
+        
+        args = column_stack((deltaTs,ones(nCases)*frequencies[i],ones(nCases)*frequencies[i],ppp))
 
-# the array has to be flipped if Cpre>Cpost 
-if synChange.Cpre>synChange.Cpost:
-    # invert time axis
-    results[:,0] = results[:,0][::-1]
-    # reorder entire array to end up with increasing time
-    results = results[::-1]
-    
+        rrr = pool.map(runIrregularPairSimulations,args)
+        #for n in range(len(frequencies)):
+        #    (synC[i,n],meanU[i,n],meanD[i,n],tD[i,n],tP[i,n]) = rrr[n]
+        #pdb.set_trace()
+        res1 = hstack((deltaTs,frequencies[i],frequencies[i],ppp,rrr))
+        results = vstack((results,res1))
 
-np.save(outputDir+'irregularSpikePairs_vs_rate_%s.npy' % params,results)
-np.savetxt(outputDir+'irregularSpikePairs_vs_rate_%s.dat' % params,results)
+    results = results[1:]
+
+    # the array has to be flipped if Cpre>Cpost 
+    if synChange.Cpre>synChange.Cpost:
+        # invert time axis
+        results[:,0] = results[:,0][::-1]
+        # reorder entire array to end up with increasing time
+        results = results[::-1]
+        
+
+    np.save(outputDir+'irregularSpikePairs_vs_rate_%s.npy' % params,results)
+    np.savetxt(outputDir+'irregularSpikePairs_vs_rate_%s.dat' % params,results)
+
+else:
+    print 'Case is not defined.'
+    print 'Possibilities are : deltaTFreq, deltaTP, freqDeltaT'
+
+
